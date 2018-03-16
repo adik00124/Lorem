a115fd6db7880e62cda8f50c96174a3584db46e5
Kacper Jachowicz <kacper.jachowicz0708@gmail.com>
2018-03-16 15:38:13 +0100

    create form

--- src/main/java/pl/adriandlugosz/loremIpsum/HomeController.java
+++ src/main/java/pl/adriandlugosz/loremIpsum/HomeController.java
@@ -1,7 +1,14 @@
 package pl.adriandlugosz.loremIpsum;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String form(){
+        return "form";
+    }
 
 
 
